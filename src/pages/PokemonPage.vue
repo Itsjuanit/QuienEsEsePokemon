01a7fe0aedcc4d837931ba73d9d615b10ee8dee4
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2022-02-25 19:19:49 -0300

    Cambio de favicon

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,17 +1,27 @@
 <template>
-  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div class="back">
+    <h1 v-if="!pokemon">Espere por favor...</h1>
 
-  <div v-else>
-    <h1>¿Quién es este pokémon?</h1>
+    <div v-else>
+      <h1>¿Quién es este Pokémon?</h1>
 
-    <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+      <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
 
-    <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+      <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
 
-    <template v-if="showAnswer">
-      <h2 class="fade-in">{{ message }}</h2>
-      <button @click="newGame">Nuevo Juego</button>
-    </template>
+      <template v-if="showAnswer">
+        <h2 class="fade-in">{{ message }}</h2>
+        <button @click="newGame" class="btn-blue-tr">Nuevo Juego</button>
+      </template>
+    </div>
+    <div class="footer">
+      <p>
+        Made with ♥ by
+        <a href="https://portfolio-itsjuanit.vercel.app/" target="_blank"
+          >Itsjuanit</a
+        >
+      </p>
+    </div>
   </div>
 </template>
 
@@ -44,9 +54,9 @@ export default {
       this.showAnswer = true;
 
       if (selectedId === this.pokemon.id) {
-        this.message = `Correcto, ${this.pokemon.name}`;
+        this.message = `Correcto, el Pokemon era ${this.pokemon.name}`;
       } else {
-        this.message = `Oops, era ${this.pokemon.name}`;
+        this.message = `Es incorrecto, el Pokemon era ${this.pokemon.name}`;
       }
     },
     newGame() {
@@ -64,10 +74,57 @@ export default {
 };
 </script>
 <style scoped>
+.back {
+  background: url("../assets/qeep.png");
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background-size: 110% 110%;
+  background-repeat: no-repeat;
+}
 div::first-letter {
   text-transform: uppercase;
 }
 h2::first-letter {
   text-transform: uppercase;
 }
+
+.btn-blue-tr {
+  border: none;
+  font-weight: bold;
+  font-size: 1rem;
+  text-align: center;
+  color: #202125;
+  cursor: pointer;
+  max-width: 200px;
+  width: 100%;
+  outline: 1px solid;
+  outline-color: rgba(71, 126, 232, 0.5);
+  outline-offset: 5px;
+  transition: all 600ms cubic-bezier(0.2, 0, 0, 0.8);
+}
+
+.btn-blue-tr:hover {
+  color: rgba(71, 126, 232, 0.7);
+  outline-color: rgba(71, 126, 232, 0);
+  outline-offset: 200px;
+  font-size: 1.5rem;
+}
+.footer {
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  font-size: 1rem;
+  color: #212121;
+  font-weight: bold;
+}
+a {
+  color: #fff;
+  text-decoration: none;
+}
+a:hover {
+  font-size: 1.2;
+}
 </style>
+
