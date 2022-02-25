2ba3ea4c1e894d056da3f97c8e4cbc258e0ed1b8
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2022-02-25 18:04:40 -0300

    Cambios en la app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PokemonPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from "./pages/PokemonPage";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PokemonPage,
+  },
+};
 </script>
 
 <style>
@@ -24,3 +23,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './css/animation.css'
+import './css/style.css'
 
 createApp(App).mount('#app')
